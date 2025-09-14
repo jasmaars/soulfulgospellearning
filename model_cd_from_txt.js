a495d364e7ba9d893d54974d8f226f8782ad52cd
jasmaars <131365820+jasmaars@users.noreply.github.com>
2025-09-13 21:39:39 -0400

    Create model_cd_from_txt.js

---
+++ model_cd_from_txt.js
@@ -0,0 +1,11 @@
+{
+  "data" : {
+    "type" : "csvfile",
+    "filename" : "cd_catalog.txt",
+    "items" : [
+      {"name" : "title", "index" : 1},
+      {"name" : "artist", "index" : 2},
+      {"name" : "price", "index" : 5}
+    ]
+  }
+}
